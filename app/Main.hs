27b7925ad4fd145458532a6acaa0ff16c5da56b0
Andrew Boltachev <andrewboltachev@gmail.com>
2022-12-01 23:07:38 +0300

    rename

--- app/Main.hs
+++ app/Main.hs
@@ -23,6 +23,7 @@ main = do
       (notFound missing)
       [ get "/" index
       , post "echo:name" echo
+      , post "python-matcher-1" pythonMatcher1
       , post "json-matcher-1" jsonMatcher1
       ]
 
@@ -36,6 +37,44 @@ echo = do
 
 jsonMatcher1 :: ResponderM a
 jsonMatcher1 = do
+  b <- (fromBody :: ResponderM Value)
+  let a = do
+      e <- (m2e "JSON root element must be a map") $ asKeyMap b
+      code <- (m2e "JSON root element must have code") $ KM.lookup (K.fromString "data") e
+      grammar <- (m2e "JSON root element must have grammar") $ KM.lookup (K.fromString "grammar") e
+      mp <- pythonMatchPattern grammar
+      let c = do
+          -- _ <- error $ show $ mp
+          r <- case matchAndCollapse mp return code of
+                  MatchFailure s -> Left ("MatchFailure: " ++ s)
+                  NoMatch x -> Left ("NoMatch: " ++ x)
+                  MatchSuccess (f, r) -> Right $ (KM.fromList [
+                    -- (K.fromString "tree", toJSON $ MatchObjectPartialResult (Object (KM.fromList [])) (KM.fromList [])),
+                    (K.fromString "grammar", toJSON $ mp),
+                    (K.fromString "funnel", (Array . V.fromList) f),
+                    (K.fromString "t1", String $ T.pack $ show mp),
+                    (K.fromString "t2", String $ T.pack $ show code),
+                    (K.fromString "result", r {-(String . T.pack) $ (show r) ++ "\n\n" ++ (show mp)-})])
+          return (Object r)
+      case c of
+           Left e -> Right $ Object (KM.fromList [
+            (K.fromString "error", (String . T.pack) ("Error: " ++ e)),
+            (K.fromString "result", Null),
+            (K.fromString "grammar", toJSON $ mp),
+            (K.fromString "funnel", Null),
+            (K.fromString "t1", String $ T.pack $ show mp),
+            (K.fromString "t2", String $ T.pack $ show code)
+            ])
+           Right r -> Right r
+  let f = case a of
+               Left _ -> status (Status {statusCode = 400, statusMessage = "request error"})
+               Right _ -> id
+  send $ f $ Web.Twain.json $ case a of
+       Left e -> Object (KM.fromList [(K.fromString "error", (String . T.pack) ("Error: " ++ e))])
+       Right x -> x
+
+pythonMatcher1 :: ResponderM a
+pythonMatcher1 = do
   b <- (fromBody :: ResponderM Value)
   let a = do
       e <- (m2e "JSON root element must be a map") $ asKeyMap b
